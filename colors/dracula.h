a23d6760979f3f86a597e2ad79f05eece6c6abc6
hossein <hosteam01@gmail.com>
2023-11-07 17:26:13 +0330

    init commit

---
+++ colors/dracula.h
@@ -0,0 +1,12 @@
+#define DARK "#191A21"
+#define LIGHT "#ffffff"
+
+static const char *colors[] = {
+	LIGHT, /* foreground color */
+	DARK, /* background color */
+};
+
+static const char *inverted_colors[] = {
+	DARK, /* foreground color */
+	LIGHT, /* background color */
+};
